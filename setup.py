3f0802f6d3ceb2b569fb96729652f6e47affe331
Simon Fester <syhester@gmail.com>
2024-06-15 21:34:11 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tsdbutils',
-    version='0.2.0',
+    name='festbotutils',
+    version='0.3.0',
     packages=find_packages(),
     description='A utility library for working with time-series databases.',
     author='Simon Fester',
     author_email='syhester@gmail.com',
-    url='https://github.com/simonfester/tsdbutils',
-    py_modules=['tsdbutils'],
+    url='https://github.com/simonfester/festbotutils',
+    py_modules=['tsdbutils','utils],
 )
