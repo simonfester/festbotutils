1b83da986d39537ff1cdd14b05e96117daaeb988
Simon Fester <syhester@gmail.com>
2024-07-23 21:00:10 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='festbotutils',
-    version='0.5.2',
+    version='0.5.4',
     packages=find_packages(),
     description='A utility library for working with time-series databases.',
     author='Simon Fester',
